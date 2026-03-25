f190e6ab198bf40d387a63fcf82ce817db10d650
Camilo Vargas <ca06vargas@gmail.com>
2026-03-24 22:53:16 -0500

    Update TypeScript types package to include schemas and adjust build configuration
    - Added support for schemas in the TypeScript types package by updating package.json to include new entry points for schemas.
    - Modified the build configuration to reference the correct schema file in tsup configuration.

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/schema.ts"],
+  entry: ["src/index.ts", "src/schemas.ts"],
   format: ["esm"],
   outDir: "dist",
   shims: true,
